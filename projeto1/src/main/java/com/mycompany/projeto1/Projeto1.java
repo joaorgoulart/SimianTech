145158392b2ea918e9d727cf65a3a05619719b70
João R. Goulart <joaorgoulartt@gmail.com>
2023-05-09 18:49:02 -0300

    initial commit

---
+++ projeto1/src/main/java/com/mycompany/projeto1/Projeto1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projeto1;
+
+
+/**
+ *
+ * @author jrgou
+ */
+public class Projeto1 {
+    
+    public static void main(String[] args) {
+         Login form = new Login();
+         form.setVisible(true);
+    }
+}
